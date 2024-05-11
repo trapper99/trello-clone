5e7c1c1ac9274c6cf534fd82a7beb6e0ee60930f
Stanley Kariuki <stanleykariuki1999@gmail.com>
2024-05-11 18:25:09 +0300

    first commit of the day

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <h1>Build a Trello Clone Home Page</h1>
+    <pre>{{ boardStore.board }}</pre>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { useBoardStore } from '~/store/boardStore';
+
+export default Vue.extend({
+  name: 'IndexPage',
+  setup() {
+    const boardStore = useBoardStore();
+
+    return {
+      boardStore,
+    }
+  }
+})
+</script>
